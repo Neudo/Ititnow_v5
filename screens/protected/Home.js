dbbd4a952eec9a3aface72f2f8b73cead51144d2
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-02-06 17:11:01 +0100

    test home

--- screens/protected/Home.js
+++ screens/protected/Home.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import {Text} from "react-native";
+import {ScrollView, Text, View, StyleSheet} from "react-native";
 
 function Home(props) {
     return (
-        <Text>Home</Text>
+        <ScrollView>
+            <View style={styles.container} >
+<Text style={styles.text} >Home</Text>
+            </View>
+
+        </ScrollView>
     );
 }
 
 export default Home;
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#f8f8ff',
+        height: 100,
+        width: "100%",
+        top: 250,
+    },
+    text: {
+        fontSize: 34,
+        fontWeight: 'bold',
+        color: '#000',
+        marginTop: 30,
+        marginHorizontal: 20,
+        textAlign: 'center',
+    }
+})
--- screens/protected/_layout.js
+++
@@ -1,8 +0,0 @@
-// import { Hub } from 'aws-amplify'
-//
-// const listener = (data) => {
-//     console.log("en bas data")
-//     console.log(data)
-// }
-//
-// Hub.listen('auth', listener)
