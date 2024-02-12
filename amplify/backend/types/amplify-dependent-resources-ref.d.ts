3743b69648532ad25833d8e31192ae3759c22b2d
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-02-12 21:10:52 +0100

    added map package

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ititnowv5a58b4a75": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
